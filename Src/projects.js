7dd34b330c8c55aaa554d559f670512f3915f17f
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2022-05-28 12:20:36 +0100

    correct punctuation

--- Src/projects.js
+++ Src/projects.js
@@ -15,37 +15,37 @@ const projectsList = [
         title: "Blurry Loading",
         url: "./Components/BlurryLoading/BlurryLoading.html",
         img: "./Images/Blurry-loading-preview.jpg",
-        description: `As the page 'loads', the background image gets sharper`
+        description: `As the page 'loads', the background image gets sharper.`
     },
     {
         title: "Hidden Search Widget",
         url: "./Components/HiddenSearchWidget/HiddenSearchWidget.html",
         img: "./Images/Hidden-search-widget-preview.jpg",
-        description: `Click on the search button to reveal a search bar`
+        description: `Click on the search button to reveal a search bar.`
     },
     {
         title: "Rotating Navigation",
         url: "./Components/RotatingNavigation/RotatingNavigation.html",
         img: "./Images/Rotating-navigation-preview.jpg",
-        description: "Click on the menu button to reveal rotating navigation"
+        description: "Click on the menu button to reveal rotating navigation."
     },
     {
         title: "Split Landing Page",
         url: "./Components/SplitLandingPage/SplitLandingPage.html",
         img: "./Images/Split-landing-page-preview.jpg",
-        description: "Sides of the page expand as you hover over them"
+        description: "Sides of the page expand as you hover over them."
     },
     {
         title: "Scroll Animation",
         url: "./Components/ScrollAnimation/ScrollAnimation.html",
         img: "./Images/Scroll-animation-preview.jpg",
-        description: "Content slides in from the sides as you scroll"
+        description: "Content slides in from the sides as you scroll."
     },
     {
         title: "Form Wave Animation",
         url: "./Components/FormWaveAnimation/FormWaveAnimation.html",
         img: "./Images/Form-wave-animation-preview.jpg",
-        description: "Labels bounce up when input field is filled or in focus"
+        description: "Labels bounce up when input field is filled or in focus."
     },
 
 ]
