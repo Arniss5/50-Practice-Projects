c3994e513e6e765c43700bdb5a856e10d152aa10
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2022-05-28 12:18:09 +0100

    Redesign index styling add project descriptions

--- Src/projects.js
+++ Src/projects.js
@@ -2,42 +2,50 @@ const projectsList = [
     {
         title: "Expanding Cards",
         url: "./Components/ExpandingCards/ExpandingCards.html",
-        img: "./Images/Expanding-cards-preview.jpg"
+        img: "./Images/Expanding-cards-preview.jpg",
+        description: "A simple responsive gallery. Click on a card to expand it."
     },
     {
         title: "Progress Steps",
         url: "./Components/ProgressSteps/ProgressSteps.html",
-        img: "./Images/Progress-steps-preview.jpg"
+        img: "./Images/Progress-steps-preview.jpg",
+        description: "Animated progress steps, useful for shopping carts, forms etc."
     },
     {
         title: "Blurry Loading",
         url: "./Components/BlurryLoading/BlurryLoading.html",
-        img: "./Images/Blurry-loading-preview.jpg"
+        img: "./Images/Blurry-loading-preview.jpg",
+        description: `As the page 'loads', the background image gets sharper`
     },
     {
         title: "Hidden Search Widget",
         url: "./Components/HiddenSearchWidget/HiddenSearchWidget.html",
-        img: "./Images/Hidden-search-widget-preview.jpg"
+        img: "./Images/Hidden-search-widget-preview.jpg",
+        description: `Click on the search button to reveal a search bar`
     },
     {
         title: "Rotating Navigation",
         url: "./Components/RotatingNavigation/RotatingNavigation.html",
-        img: "./Images/Rotating-navigation-preview.jpg"
+        img: "./Images/Rotating-navigation-preview.jpg",
+        description: "Click on the menu button to reveal rotating navigation"
     },
     {
         title: "Split Landing Page",
         url: "./Components/SplitLandingPage/SplitLandingPage.html",
-        img: "./Images/Split-landing-page-preview.jpg"
+        img: "./Images/Split-landing-page-preview.jpg",
+        description: "Sides of the page expand as you hover over them"
     },
     {
         title: "Scroll Animation",
         url: "./Components/ScrollAnimation/ScrollAnimation.html",
-        img: "./Images/Scroll-animation-preview.jpg"
+        img: "./Images/Scroll-animation-preview.jpg",
+        description: "Content slides in from the sides as you scroll"
     },
     {
         title: "Form Wave Animation",
         url: "./Components/FormWaveAnimation/FormWaveAnimation.html",
-        img: "./Images/Form-wave-animation-preview.jpg"
+        img: "./Images/Form-wave-animation-preview.jpg",
+        description: "Labels bounce up when input field is filled or in focus"
     },
 
 ]
--- script.js
+++ script.js
@@ -4,7 +4,19 @@ import projectsList from "./Src/projects.js";
 const projects = document.querySelector('.projects')
 
 projectsList.forEach(project => {
-    const elementsToAdd = `<li class="projects--project"><a href=${project.url} class="projects--link"><img src=${project.img} class="projects--img"/><h2 class="project--title">${project.title}</h2></a></li>`
+    const elementsToAdd =
+     `<section class="project--card">
+        <a href=${project.url} class="project--link">
+            <img src=${project.img} class="project--img"/>
+            <h2 class="project--title">${project.title}</h2>
+            <p class="project--description">${project.description}</p>
+        </a>
+    </section>`
     projects.innerHTML += elementsToAdd
 
 });
+
+//li => section
+// projects--project => project-card
+// projects--img => project--img
+// 
