f3c6f3921e9f3b81b525ef2df1cbfef4e01bac66
Arniss5 <86066404+Arniss5@users.noreply.github.com>
2022-06-05 16:38:54 +0100

    add loader to Dad Jokes

--- Components/DadJokes/script.js
+++ Components/DadJokes/script.js
@@ -1,5 +1,6 @@
 const joke = document.querySelector('.joke')
 const button = document.querySelector('.next-btn')
+const loader = document.querySelector('.loader')
 
 
 const config = {
@@ -11,12 +12,23 @@ const config = {
 
 function generateJoke() {
     fetch('https://icanhazdadjoke.com/', config)
-    .then(response => response.json())
+    .then(response => {
+        loading()
+        return response.json()})
     .then(data => {
         joke.innerText = data.joke
+        complete()
     })
 }
 
 generateJoke()
 
 button.addEventListener('click', generateJoke)
+
+function loading() {
+    loader.classList.remove('complete')
+}
+
+function complete() {
+    loader.classList.add('complete')
+}
